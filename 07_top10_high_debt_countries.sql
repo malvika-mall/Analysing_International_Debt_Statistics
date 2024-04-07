d98dbae155e878571aa1ceed679b99999e8b98d4
Malvika Mall <malvika.nimoria@gmail.com>
2024-04-07 22:41:59 +0200

    Delete 07_top10_high_debt_countries.sql

--- 07_top10_high_debt_countries.sql
+++
@@ -1,9 +0,0 @@
---top 10 countries with maximum debt
-
-SELECT
-    country_name,
-    MAX(debt) AS maximum_debt
-FROM international_debt
-GROUP BY country_name
-ORDER BY maximum_debt DESC
-LIMIT 10;
